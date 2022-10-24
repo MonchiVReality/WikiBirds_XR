62b3352278740b2e52df152c7253ed8e4c6a93ce
javimu06 <javimu06@ucm.es>
2022-10-25 00:08:41 +0200

    VR Camera Implemented

--- WikiBirds_XR_Proyect/Assets/My Assets/Own_Scripts/takePhotograph.cs
+++ WikiBirds_XR_Proyect/Assets/My Assets/Own_Scripts/takePhotograph.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class takePhotograph : MonoBehaviour
 {
@@ -17,20 +18,49 @@ public class takePhotograph : MonoBehaviour
 
     private bool viewingPhoto;
 
-    private void Start()
-    {  
+    //Photo prefab
+    public GameObject picture_;
+    private GameObject spawnedPhoto;
+    public Transform photoSpawnPoint_;
+    bool isSocketActive = false;
+    public void changeSocketActive(bool aux = false)
+    {
+        isSocketActive = aux;
+        if (isSocketActive)
+            spawnedPhoto.transform.tag = "Photo";
+        else
+            spawnedPhoto.transform.tag = "Untagged";
     }
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //Take screenshot
-            if (!viewingPhoto)
-                StartCoroutine(CapturePhoto());
-            else
-                removePhoto();
-        }
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(TakePhoto);
+    }
+
+    //private void Update()
+    //{
+    //    //INPUT ON KEYBOARD
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        //Take screenshot
+    //        if (!viewingPhoto)
+    //            StartCoroutine(CapturePhoto());
+    //        else
+    //            removePhoto();
+    //    }
+    //}
+
+    // XR INPUT
+    public void TakePhoto(ActivateEventArgs arg)
+    {
+        if (!isSocketActive)
+            spawnedPhoto = Instantiate(picture_);
+
+        isSocketActive = true;
+        spawnedPhoto.SetActive(true);
+        spawnedPhoto.transform.position = photoSpawnPoint_.position;
+        StartCoroutine(CapturePhoto());
     }
 
     IEnumerator CapturePhoto()
@@ -42,35 +72,36 @@ public class takePhotograph : MonoBehaviour
     void showPhoto()
     {
         viewingPhoto = true;
-        photoFrame.SetActive(true);
-        photoDisplayArea.SetActive(true);
+        //photoFrame.SetActive(true);
+        //photoDisplayArea.SetActive(true);
 
         RenderTexture renderTexture = new RenderTexture(width, height, depth);
         Rect rect = new Rect(0, 0, width, height);
         Texture2D texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
 
-        GetComponent<Camera>().targetTexture = renderTexture;
-        GetComponent<Camera>().Render();
+        camera_.targetTexture = renderTexture;
+        camera_.Render();
 
         RenderTexture currentRenderTexture = RenderTexture.active;
         RenderTexture.active = renderTexture;
         texture.ReadPixels(rect, 0, 0);
         texture.Apply();
 
-        GetComponent<Camera>().targetTexture = null;
+        camera_.targetTexture = null;
         RenderTexture.active = currentRenderTexture;
         Destroy(renderTexture);
 
         Sprite sprite = Sprite.Create(texture, rect, Vector2.zero);
 
-        photoDisplayArea.GetComponent<Image>().sprite = sprite;
+        //photoDisplayArea.GetComponent<Image>().sprite = sprite;
 
+        spawnedPhoto.GetComponent<MeshRenderer>().material.mainTexture = texture;
     }
 
     void removePhoto()
     {
         viewingPhoto = false;
-        photoFrame.SetActive(false);
-        photoDisplayArea.SetActive(false);
+        //photoFrame.SetActive(false);
+        //photoDisplayArea.SetActive(false);
     }
 }
